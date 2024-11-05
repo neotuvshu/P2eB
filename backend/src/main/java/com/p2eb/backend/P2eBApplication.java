e71d56e0c424adfb7b47e8cc9b644bb76f3c5268
neotuvshu <neotuvshu@gmail.com>
2024-11-05 15:20:45 +0800

    backendsteup

---
+++ backend/src/main/java/com/p2eb/backend/P2eBApplication.java
@@ -0,0 +1,13 @@
+package com.p2eb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2eBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2eBApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/p2eb/backend/P2eBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2eb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2eBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
